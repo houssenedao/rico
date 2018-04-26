2b294df51d1ed4ca17a219ba2821cc419c73a899
Houssene Dao <dao.houssene@gmail.com>
2018-04-26 12:27:02 +0000

    update phpunit.xml

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/houssenedao/rico.svg?branch=master)](https://travis-ci.org/houssenedao/rico)
+
 # Rico
 PHP realtime server
 
--- tests/Unit/RicoTest.php
+++ tests/Unit/RicoTest.php
@@ -1,1 +1,13 @@
 <?php
+
+use PHPUnit\Framework\TestCase;
+
+class RicoTest extends TestCase
+{
+
+    /** @test */
+    public function sampleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
